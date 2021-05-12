2c6e53f5c95ad6b64c03eaea23456ecc6630cb20
RM <70502707+reyad0606@users.noreply.github.com>
2021-05-12 17:12:17 -0500

    ..

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const data = require('./questions.json');
+
+//Routes
+app.get('/', (req, res) => {
+  res.send('We are on Quiz Home Page');
+});
+
+app.get('/questions', async (req, res) => {
+  try {
+    res.json(data);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+app.listen(3005);
